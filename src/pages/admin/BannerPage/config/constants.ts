9fabad35e89426ef7176b3ee1df6d7236ddbca3d
beksultan <hfjcgyrishwidhzhd1038@gmail.com>
2024-05-28 22:38:39 +0600

    complete

--- src/pages/admin/BannerPage/config/constants.ts
+++ src/pages/admin/BannerPage/config/constants.ts
@@ -5,7 +5,7 @@ export const banners = [
       'https://avatars.mds.yandex.net/i?id=e805734a3041b629e1ae289681850975a7a7fe33-12498917-images-thumbs&n=13',
     load_date: '22.11.2024',
     size: '578 кб',
-		time: '0:21'
+    time: '0:21',
   },
   {
     id: 2,
