1d8c4b42aed30c1fed73d50d321ff19e9b50b8ab
sanjar <san4o.myrzakmatov@gmail.com>
2024-06-08 21:52:40 +0600

    adminHead

--- src/widgets/AdminHeader/ui/Calendar.tsx
+++ src/widgets/AdminHeader/ui/Calendar.tsx
@@ -1,8 +1,9 @@
 import { FC, useEffect, useState } from 'react';
-import { addDays, format, subDays } from 'date-fns';
+import { format } from 'date-fns';
 import moment from 'moment-timezone';
 import { ru } from 'date-fns/locale';
 import AdminCalendar from '../../../features/AdminCalendar/ui/AdminCalendar';
+import { CaretDown, CaretUp } from '@phosphor-icons/react';
 
 export const Calendar: FC = () => {
   const [currentDate, setCurrentDate] = useState<Date | null>(null);
@@ -14,43 +15,53 @@ export const Calendar: FC = () => {
     setCurrentDate(bishkekTime);
   }, []);
 
-  const goToPreviousDay = () => {
-    if (currentDate) {
-      setCurrentDate(subDays(currentDate, 1));
-    }
-  };
-
-  const goToNextDay = () => {
-    if (currentDate) {
-      setCurrentDate(addDays(currentDate, 1));
-    }
-  };
+  // const goToPreviousDay = () => {
+  //   if (currentDate) {
+  //     setCurrentDate(subDays(currentDate, 1));
+  //   }
+  // };
+  //
+  // const goToNextDay = () => {
+  //   if (currentDate) {
+  //     setCurrentDate(addDays(currentDate, 1));
+  //   }
+  // };
 
   return (
     <>
       {currentDate ? (
         <>
-          <div className="w-[350px] h-[47px] flex justify-center items-center bg-[#FDFDFD] rounded-[8px]">
-            <button onClick={goToPreviousDay}>
-              <img
-                src="/admin-arrow-right.svg"
-                className="rotate-180 p-3"
-                alt="arrow-left"
-              />
-            </button>
+          <div className="w-[220px] py-[5px] flex justify-center items-center bg-[#2B2B2B] rounded-[8px] text-white">
+            {/*<button onClick={goToPreviousDay}>*/}
+            {/*  <CaretLeft size={30} />*/}
+            {/*</button>*/}
             <div
               onClick={() => setCalendar(!calendar)}
               className="font-comfortaa text-2xl font-bold leading-[26.76px] text-center px-[22px] pb-[6px] m-auto cursor-pointer"
             >
-              {format(currentDate, 'E, dd MMMM', { locale: ru })}
+              <h1 className="text-[16px] uppercase">
+                {format(currentDate, 'EEEE', { locale: ru })}
+              </h1>
+              <h1 className="text-[#C1C1C1] text-[15px]">
+                {format(currentDate, 'dd MMMM', { locale: ru })}
+              </h1>
             </div>
-            <button onClick={goToNextDay}>
-              <img
-                src="/admin-arrow-right.svg"
-                alt="arrow-right"
-                className="p-3"
+            {calendar ? (
+              <CaretUp
+                size={26}
+                className="mr-[10px]"
+                onClick={() => setCalendar(!calendar)}
               />
-            </button>
+            ) : (
+              <CaretDown
+                size={26}
+                className="mr-[10px]"
+                onClick={() => setCalendar(!calendar)}
+              />
+            )}
+            {/*<button onClick={goToNextDay}>*/}
+            {/*  <CaretRight size={30} />*/}
+            {/*</button>*/}
           </div>
         </>
       ) : (
--- src/widgets/AdminHeader/ui/FilterButton.tsx
+++ src/widgets/AdminHeader/ui/FilterButton.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { FILTERED_BTN } from '../config/constants';
+import { CaretLeft, CaretRight, MagnifyingGlass } from '@phosphor-icons/react';
 
 interface Props {
   setAddModal: (modal: boolean) => void;
@@ -7,39 +8,43 @@ interface Props {
   modal: boolean;
 }
 export const FilterButton: FC<Props> = ({ setAddModal, setModal, modal }) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const openModal = () => {
     setAddModal(true);
     setModal(!modal);
   };
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % FILTERED_BTN.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + FILTERED_BTN.length) % FILTERED_BTN.length,
+    );
+  };
+
   return (
     <>
-      <div className="h-[47px] px-[12px] flex items-center gap-[10px] rounded-[8px] bg-[#FDFDFD]">
-        {FILTERED_BTN?.map((item) => (
-          <div key={item.title}>
-            <button className="rounded-[8px] p-[8px]  hover:bg-[black] hover:text-white">
-              {item.title}
-            </button>
-          </div>
-        ))}
-      </div>
-      <div className="w-[345px] h-[47px] flex rounded-[8px] bg-[#FDFDFD]">
-        <div className="w-[12%] m-auto">
-          <img
-            src="/search-normal.svg"
-            alt="search"
-            className="w-[18px] h-[24px] m-auto"
-          />
+      <div className="h-[47px] w-[850px] px-[12px] flex items-center justify-center gap-[10px] rounded-[8px] bg-black text-white">
+        <CaretLeft size={30} onClick={handlePrev} />
+        <div className="w-[170px] text-center text-[20px]">
+          {FILTERED_BTN[currentIndex].title}
         </div>
+        <CaretRight size={30} onClick={handleNext} />
+      </div>
+      <div className="w-[150px] h-[47px] flex rounded-[8px] bg-[#2B2B2B] items-center justify-center">
+        <MagnifyingGlass className="w-[20px] h-[20px] text-white ml-[10px]" />
         <input
           type="text"
           placeholder="Поиск"
-          className="w-[90%] py-[10px] outline-none bg-[#FDFDFD] rounded-[8px]"
+          className="w-[90%] py-[10px] outline-none bg-[#2B2B2B] rounded-[8px] text-white pl-[5px]"
         />
       </div>
       <div>
         <button
           onClick={openModal}
-          className="h-[47px] px-[32px] py-[12px] rounded-[8px] bg-[#FDFDFD]"
+          className="h-[47px] px-[32px] py-[12px] rounded-[8px] bg-[#6BC678] text-white"
         >
           Добавить +
         </button>
