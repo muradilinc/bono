64a8cf7779030fa4a9cd3c2eecf64cf53bc0bdba
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-06-10 13:58:13 +0600

    fix to show date

--- src/features/shedule/api/scheduleThunk.ts
+++ src/features/shedule/api/scheduleThunk.ts
@@ -13,15 +13,14 @@ export const createBook = createAsyncThunk<void, IForms>(
   async (book) => {
     const endTime = parseInt(book.time) + parseInt(book.timeA);
     await axiosApi.post('/book/create/book/', {
-      title: 'Amount',
       user_name: book.name,
       phone_number: '+' + book.tel,
-      time_stamp: book.time,
-      will_come: dayjs(new Date()).format('YYYY-MM-DD'),
+      time_stamp: book.time, // кол-во время проведения с эскорт
+      will_come: dayjs(new Date()).format('YYYY-MM-DD'), // дата когда гость прибудет
       start_time: book.time,
       end_time: endTime.toString() + ':00',
       amount_guest: book.guests,
-      table: 2,
+      table: 2, // номер столик
       comment: book.comments,
     });
   },
