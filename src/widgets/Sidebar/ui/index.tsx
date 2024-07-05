769367e9d2d6561896e5f50e6d1df36f287b0fb1
sanjar <san4o.myrzakmatov@gmail.com>
2024-07-05 15:43:49 +0600

    fixSidebar

--- src/widgets/Sidebar/ui/index.tsx
+++ src/widgets/Sidebar/ui/index.tsx
@@ -1,31 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { links } from '../../../app/constants/links';
-import { useState } from 'react';
-import { useAppSelector } from '../../../app/store/hooks';
-import { selectBannersLoading } from '../../../features/banner/model/bannerSlice';
-import Loading from '../../../shared/ui/Loading';
-import { selectCategoriesLoading } from '../../../features/category/categorySlice';
-import { selectMenuLoading } from '../../../features/AdminFilterMenu/model/MenuSlica';
-import { selectSchedulesLoading } from '../../../features/shedule/model/scheduleSlice';
-import { selectSubCategoriesLoading } from '../../../pages/admin/SubCategoryPage/model/subCategorySlice';
 
 export const Sidebar = () => {
-  const [active, setActive] = useState<number | null>(null);
-  const loadingBanner = useAppSelector(selectBannersLoading);
-  const loadingCatalog = useAppSelector(selectCategoriesLoading);
-  const loadingMenu = useAppSelector(selectMenuLoading);
-  const loadingIncoming = useAppSelector(selectSchedulesLoading);
-  const loadingSubCategory = useAppSelector(selectSubCategoriesLoading);
+  const params = useParams();
 
-  const isLoading =
-    loadingBanner ||
-    loadingCatalog ||
-    loadingMenu ||
-    loadingIncoming ||
-    loadingSubCategory;
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
     <aside className="w-full max-w-[210px] flex flex-col gap-[50px] bg-[#2B2B2B]">
       <div className="bg-[#2B2B2B] p-[16px] flex items-center">
@@ -36,8 +14,7 @@ export const Sidebar = () => {
           {links.map((link, idx) => (
             <Link to={link.path} key={idx}>
               <li
-                onClick={() => setActive(idx)}
-                className={`${active === idx ? 'border-b-2 border-white' : ''} p-[10px] hover:bg-[#3D3D3D] hover:opacity-100 rounded-[8px] duration-300 font-medium opacity-90 text-white`}
+                className={`${Object.values(params)[0] === link.path.split('/')[2] ? 'border-b-2 border-white' : ''} p-[10px] hover:bg-[#3D3D3D] hover:opacity-100 rounded-[8px] duration-300 font-medium opacity-90 text-white`}
               >
                 {link.name}
               </li>
