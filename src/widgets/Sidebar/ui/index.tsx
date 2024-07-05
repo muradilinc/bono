54b96229d57f8d28f1e8c611175ea5910c7f193b
Muradil <muradil.koychubekob@gmail.com>
2024-07-05 14:00:57 +0600

    fix sidebar

--- src/widgets/Sidebar/ui/index.tsx
+++ src/widgets/Sidebar/ui/index.tsx
@@ -1,22 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { links } from '../../../app/constants/links';
-import { useState } from 'react';
-import { useAppSelector } from '../../../app/store/hooks';
-import { selectBannersLoading } from '../../../features/banner/model/bannerSlice';
-import Loading from '../../../shared/ui/Loading';
-import { selectCategoriesLoading } from '../../../features/category/categorySlice';
-import { selectMenuLoading } from '../../../features/AdminFilterMenu/model/MenuSlica';
 
 export const Sidebar = () => {
-  const [active, setActive] = useState<number | null>(null);
-  const loadingBanner = useAppSelector(selectBannersLoading);
-  const loadingCatalog = useAppSelector(selectCategoriesLoading);
-  const loadingMenu = useAppSelector(selectMenuLoading);
+  const params = useParams();
 
-  const isLoading = loadingBanner || loadingCatalog || loadingMenu;
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
     <aside className="w-full max-w-[210px] flex flex-col gap-[50px] bg-[#2B2B2B]">
       <div className="bg-[#2B2B2B] p-[16px] flex items-center">
@@ -25,13 +12,13 @@ export const Sidebar = () => {
       <div className="p-[16px] flex flex-col gap-[20px]">
         <ul className="flex flex-col gap-[10px]">
           {links.map((link, idx) => (
-            <li
-              onClick={() => setActive(idx)}
-              className={`${active === idx ? 'border-b-2 border-white' : ''} p-[10px] hover:bg-[#3D3D3D] hover:opacity-100 rounded-[8px] duration-300 font-medium opacity-90 text-white`}
-              key={idx}
-            >
-              <Link to={link.path}>{link.name}</Link>
-            </li>
+            <Link to={link.path} key={idx}>
+              <li
+                className={`${Object.values(params)[0] === link.path.split('/')[2] ? 'border-b-2 border-white' : ''} p-[10px] hover:bg-[#3D3D3D] hover:opacity-100 rounded-[8px] duration-300 font-medium opacity-90 text-white`}
+              >
+                {link.name}
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
