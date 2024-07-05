cff6cc46c577adf2d220d8f6e2c48dfd92ccce24
Muradil <muradil.koychubekob@gmail.com>
2024-07-05 18:07:56 +0600

    connect filter by date and floor

--- src/features/AdminCalendar/ui/AdminCalendar.tsx
+++ src/features/AdminCalendar/ui/AdminCalendar.tsx
@@ -2,23 +2,32 @@ import { FC } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 interface PropsType {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  setCurrentDate: any;
-  currentDate: Date | null;
+  setCurrentDate: (date: Value) => void;
+  currentDate: Value;
   setCalendar: (value: boolean) => void;
+  setFilterDate: (date: Value) => void;
 }
 
 const AdminCalendar: FC<PropsType> = ({
   setCurrentDate,
   currentDate,
   setCalendar,
+  setFilterDate,
 }) => {
+  const changeDate = (value: Value) => {
+    setCurrentDate(value);
+    setFilterDate(value);
+  };
   return (
     <div className="p-4 absolute top-14 left-0 z-30">
       <Calendar
         className="bg-[#2B2B2B] text-white"
-        onChange={setCurrentDate}
+        onChange={changeDate}
         value={currentDate}
         onClickDay={() => setCalendar(false)}
       />
