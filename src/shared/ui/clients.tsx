4d6f14e930a914bc31bfab5ea7bc6a6704c12ffb
Muradil <muradil.koychubekob@gmail.com>
2024-07-07 22:27:37 +0600

    show text "empty" if incoming list is empty

--- src/shared/ui/clients.tsx
+++ src/shared/ui/clients.tsx
@@ -11,21 +11,25 @@ const Clients: React.FC<Props> = ({ currentClient }) => {
   const filterBook = books.filter((book) => book.table === null);
 
   return (
-    <div className="flex flex-col gap-y-5">
-      {filterBook.map((client) => (
-        <div
-          key={client.id}
-          onClick={() => currentClient(client.id)}
-          className="flex gap-x-3 text-white px-[15px] py-[10px] border border-white cursor-pointer"
-        >
-          <p>{client.user_name}</p>
-          <p>{client.phone_number}</p>
-          <p>{client.will_come}</p>
-          <p>{client.start_time}</p>
-          <p>{client.end_time}</p>
-          <p>{client.time_stamp}</p>
-        </div>
-      ))}
+    <div className="flex flex-col gap-y-5 text-white min-w-[300px]">
+      {filterBook.length > 0 ? (
+        filterBook.map((client) => (
+          <div
+            key={client.id}
+            onClick={() => currentClient(client.id)}
+            className="flex gap-x-3 text-white px-[15px] py-[10px] border border-white cursor-pointer"
+          >
+            <p>{client.user_name}</p>
+            <p>{client.phone_number}</p>
+            <p>{client.will_come}</p>
+            <p>{client.start_time}</p>
+            <p>{client.end_time}</p>
+            <p>{client.time_stamp}</p>
+          </div>
+        ))
+      ) : (
+        <h4 className="text-center">Пусто</h4>
+      )}
     </div>
   );
 };
