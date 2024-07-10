8eb214122a72af9c29d8feb7db781fb2edb117a9
sanjar <san4o.myrzakmatov@gmail.com>
2024-07-10 19:37:08 +0600

    fix-common

--- src/widgets/formCome/ui/index.tsx
+++ src/widgets/formCome/ui/index.tsx
@@ -154,7 +154,7 @@ export const FormCome = () => {
                 className="bg-transparent border-b w-full p-[10px] border-white"
               >
                 <option className="bg-black" disabled value="">
-                  select
+                  Выбрать
                 </option>
                 {times.map((time, inx) => (
                   <option key={inx} className="bg-black" value={time}>
