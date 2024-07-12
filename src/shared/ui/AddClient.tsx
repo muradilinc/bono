bd45637028780a76ace1d23ae8d4f74cb1b72690
russllan <rahmatov.ruslan.02@bk.ru>
2024-07-13 01:30:00 +0600

    fix: validation of the time for exceeding 4 o'clock in the morning

--- src/shared/ui/AddClient.tsx
+++ src/shared/ui/AddClient.tsx
@@ -129,9 +129,38 @@ const AddClient: React.FC<Props> = ({ onClose, id, filter }) => {
     }
   };
 
+  const handleTimeValidation = () => {
+    const startTime = form.start_time; // "10:00"
+    const hoursToAdd = form.time_stamp; // 1
+    const [startHours, startMinutes] = startTime.split(':').map(Number);
+    let newHours = (startHours + Number(hoursToAdd)) % 24;
+    const newMinutes = startMinutes;
+    if (newHours < 0) {
+      newHours += 24;
+    }
+    const newTime = `${String(newHours).padStart(2, '0')}:${String(newMinutes).padStart(2, '0')}`;
+    console.log('00:00 = ', startTime);
+    console.log('0 h = ', hoursToAdd);
+    console.log('New time = ', newTime);
+    const newTimeInMinutes = newHours * 60 + newMinutes;
+    const fourAMInMinutes = 4 * 60;
+    if (newTimeInMinutes >= fourAMInMinutes) {
+      toast.error('Время превышает 4:00 утра!!!');
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (handleTimeValidation()) {
+      await addClient(event);
+    }
+  };
+
   return (
     <form
-      onSubmit={addClient}
+      onSubmit={handleSubmit}
       className="flex flex-col gap-[20px] mt-[20px] text-white"
     >
       <div className="flex items-center justify-between gap-3">
