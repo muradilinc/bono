b5786a9e30df82e6673e7c39260c7b968ea238d7
sanjar <san4o.myrzakmatov@gmail.com>
2024-07-15 01:22:17 +0600

    seo

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store/store';
-import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
