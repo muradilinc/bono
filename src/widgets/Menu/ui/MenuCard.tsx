f3f0c6e83c1dcb3a21ec6b9288c4943c7b06b6b7
sanjar <san4o.myrzakmatov@gmail.com>
2024-07-17 17:23:10 +0600

    order

--- src/widgets/Menu/ui/MenuCard.tsx
+++ src/widgets/Menu/ui/MenuCard.tsx
@@ -51,10 +51,8 @@ const Card: FC<{ item: MenuType; variants: Variants }> = ({
         className="absolute w-full bottom-0 bg-[rgba(23,23,23,0.6)] p-[10px]"
       >
         <div className="flex items-center justify-between">
-          <h3 className="text-[16px] font-semibold">{item.title}</h3>
-          <h3 className="text-[16px] font-semibold text-nowrap">
-            {item.price} с
-          </h3>
+          <h3 className="text-[16px]">{item.title}</h3>
+          <h3 className="text-[16px] text-nowrap">{item.price} с</h3>
         </div>
         <p className="text-[12px] text-wrap">{item.description}</p>
       </motion.div>
